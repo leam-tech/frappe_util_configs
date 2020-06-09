2d289885290d114c52cebad9479e0927cf71cb0d
Fahim Ali Zain <faztp12@gmail.com>
2020-06-09 16:23:25 +0200

    JWT Updates

--- frappe_util_configs/auth.py
+++ frappe_util_configs/auth.py
@@ -57,37 +57,33 @@ class RenovationHTTPRequest(HTTPRequest):
 		elif frappe.request.path.startswith('/private/files/') and frappe.request.args.get("token"):
 			jwt_token = frappe.request.args.get("token")
 
-		if jwt_token:
-			frappe.flags.jwt = jwt_token
-			token_info = jwt.decode(jwt_token, frappe.utils.password.get_encryption_key())
-
-			# Not checking by IP since it could change on network change (Wifi -> Mobile Network)
-			# if token_info.get('ip') != frappe.local.request_ip:
-			# 	frappe.throw(frappe._("Invalide IP", frappe.AuthenticationError))
-
-			# werkzueg cookies structure is immutable
-			frappe.request.cookies = frappe._dict(frappe.request.cookies)
-			frappe.request.cookies['sid'] = token_info.get('sid')
-
-
-		# load cookies
-		frappe.local.cookie_manager = CookieManagerJWT()
-
-		# login
-		frappe.local.login_manager = LoginManager()
-
 		if frappe.form_dict._lang:
 			lang = get_lang_code(frappe.form_dict._lang)
 			if lang:
 				frappe.local.lang = lang
 
-		self.validate_csrf_token()
+		if jwt_token:
+			frappe.flags.jwt = jwt_token
+			token_info = jwt.decode(jwt_token, frappe.utils.password.get_encryption_key())
 
-		# write out latest cookies
-		frappe.local.cookie_manager.init_cookies()
+			# set user resets frappe.local.form_dict
+			form_dict = frappe.local.form_dict
+			frappe.set_user(token_info.get("user"))
+			frappe.local.form_dict = form_dict
+		else:
+			# load cookies
+			frappe.local.cookie_manager = CookieManagerJWT()
 
-		# check status
-		check_session_stopped()
+			# login
+			frappe.local.login_manager = LoginManager()
+
+			self.validate_csrf_token()
+
+			# write out latest cookies
+			frappe.local.cookie_manager.init_cookies()
+
+			# check status
+			check_session_stopped()
 
 
 def make_jwt(user, expire_on=None, secret=None):
@@ -103,12 +99,15 @@ def make_jwt(user, expire_on=None, secret=None):
 		"alg": "HS256"
 	}
 	id_token = {
-		"sub": user,
-		"ip": frappe.local.request_ip,
-		"sid": frappe.session.get('sid')
+		"user": user,
+		"ip": frappe.local.request_ip
 	}
 	if expire_on:
 		id_token['exp'] = int((expire_on - frappe.utils.datetime.datetime(1970, 1, 1)).total_seconds())
 	token_encoded = jwt.encode(id_token, secret, algorithm='HS256', headers=id_token_header).decode("ascii")
 	frappe.flags.jwt = token_encoded
+
+	from frappe.sessions import delete_session
+	delete_session(frappe.session.get("sid"), user=user, reason="Session Deleted for JWT")
+
 	return token_encoded
